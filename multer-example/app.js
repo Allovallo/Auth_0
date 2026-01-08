9da4385600b03df824bbd0a72f1214173fb9fe5b
grossco <30566134+Allovallo@users.noreply.github.com>
2026-01-08 16:15:13 +0200

    files updated

--- multer-example/app.js
+++ multer-example/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs/promises");
 
 const app = express();
 
@@ -9,9 +10,11 @@ app.use(cors());
 app.use(express.json());
 
 const tempDir = path.join(__dirname, "temp");
+console.log(__dirname);
 
 const multerConfig = multer.diskStorage({
-  destinantion: tempDir,
+  // destinantion: tempDir,
+  destination: (req, file, cb) => cb(null, tempDir),
   filename: (req, file, cb) => {
     cb(null, file.originalname);
   },
@@ -25,9 +28,14 @@ app.get("/api/books", (req, res) => {
   res.json(books);
 });
 
+// upload.fields([{name: 'cover', maxCount: 1}, {name: 'subcover', maxCount: 2}])
+// upload.array('cover', 8)
+
+
 app.post("/api/books", upload.single("cover"), async (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
+  // console.log(req.body);
+  // console.log(req.file);
+  await fs.rename("./temp/cover.jpg", "./public/books/cover.jpg");
 });
 
 app.listen(3000);
